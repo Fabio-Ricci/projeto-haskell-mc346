8fb024d75d02363107f55f6d1d33067f972e6ae7
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2018-09-25 19:26:27 -0300

    fix

--- main.hs
+++ main.hs
@@ -2,35 +2,11 @@ import System.IO
 import System.Environment     
 import Data.List
 
-
--- entrada:
-
--- a b a-pe 0.4 (origem, destino, metodo, tempo)
--- b a a-pe 0.6 (arestas direcionais, b para a demora mais que a para b)
--- b c a-pe 0.5 
--- c b a-pe 0.5
--- c d a-pe 0.3
--- d c a-pe 0.3
--- a d linha-370 0.1
--- d f a-pe 3
--- f h linha-567 1.2
--- f h a-pe 12.3
--- (linha em branco separa o grafo das informações das linhas)
--- linha-370 15.0 (linha, e periodo de passagem do onibus na linha)
--- linha-567 12.0 (custo de esperar o onibus é fixo e metade do periodo (6.00 no caso))
--- (linha em branco separa as linhas da origem/destino)
--- a h (origem, destino)
-
--- saída:
--- a a-pe b a-pe c linha-370 f a-pe h a-pe i (caminho)
--- 17.5 (tempo total)
 data Aresta = Aresta {
     origem::String,
     destino::String,
-    -- origem::No,
-    -- destino::No,
     metodo::String,
-    peso:: Float -- mudar para int
+    peso:: Float
 } deriving (Eq,Show,Read)
 
 data No = No {
